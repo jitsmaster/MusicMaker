2b2cfcc9b05d0d2b6b32b7921f11b42a507ae4e7
Arnold Wang <arnold.shu.wang@gmail.com>
2017-10-20 07:46:02 -0700

    Added catslevania and contra last stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,10 @@ import { Notes_Zelda } from "./notes/zelda/zelda";
 import { Notes_Mario } from "./notes/mario/mario";
 import { Notes_FF } from "./notes/ff/ff";
 import { Notes_Contra } from "./notes/contra/contra";
+import { Notes_Contra2 } from "./notes/contra2/contra2"
 import { Notes_LifeForce } from "./notes/lifeforce/lifeForce";
+import { Notes_DoubleDragon } from "./notes/dougledragon/dd";
+import { Notes_Castlevania } from "./notes/castlevania/castlevania";
 
 @Component({
     selector: 'app-root',
@@ -53,9 +56,15 @@ export class AppComponent {
             song = Notes_FF;
         else if (this.songName == "Contra")
             song = Notes_Contra;
-        else if (this.songName == "Salamander")
+            else if (this.songName == "Contra2")
+            song = Notes_Contra2;
+        else if (this.songName == "Salamander")     
             song = Notes_LifeForce;
-
+        else if (this.songName == "dd")     
+            song = Notes_DoubleDragon;
+        else if (this.songName == "castlavania")
+            song = Notes_Castlevania;
+        
         if (this.playing)
             this.player.play(song);
         else
---
+++ src/app/notes/castlevania/castlevania.ts
@@ -0,0 +1,13 @@
+import { Song } from "../song";
+
+import { bass1 } from "./bass1";
+import { bass2 } from "./bass2";
+import { synth1 } from "./synth1";
+import { synth2 } from "./synth2";
+
+export let Notes_Castlevania  = {
+	triangle : bass1,
+	noise : bass2,
+	pulse: synth1,
+	square: synth2
+} as Song;
